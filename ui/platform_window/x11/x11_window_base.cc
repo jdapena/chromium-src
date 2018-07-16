jesonlay/chromium

[ozone/x11] Add popup window type support.
This commit adds a popup window type to ozone windows.
Popup windows are drop down windows, for example.

Issue #417

fixup! Add popup window type support.

It added case handling on WindowTreeHostPlatform::GetWindowType().

ui/platform_window/x11/x11_window_base.cc
@@ -131,15 +131,18 @@ void X11WindowBase::Create() {
   ui::PlatformWindowType ui_window_type =
       ui::PlatformWindowType::PLATFORM_WINDOW_TYPE_WINDOW;
   // TODO(msisov, jkim): pass PlatformWindowInitProperties here.
-  if (ui_window_type != ui::PlatformWindowType::PLATFORM_WINDOW_TYPE_WINDOW) {
-    // Setting this to True, doesn't allow X server to set different
-    // properties, e.g. decorations.
-    // TODO(msisov): Investigate further.
-    // https://tronche.com/gui/x/xlib/window/attributes/override-redirect.html
-    swa.override_redirect = x11::True;
-    window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_MENU");
-  } else {
-    window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_NORMAL");
+   switch (ui_window_type) {
+    case ui::PlatformWindowType::PLATFORM_WINDOW_TYPE_MENU:
+      window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_MENU");
+      swa.override_redirect = x11::True;
+      break;
+    case ui::PlatformWindowType::PLATFORM_WINDOW_TYPE_POPUP:
+      swa.override_redirect = x11::True;
+      window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_NOTIFICATION");
+      break;
+    default:
+      window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_NORMAL");
+      break;
   }
 
   xwindow_ =

jdapena/chromium-src

ui/platform_window/x11/x11_window_base.cc
@@ -130,15 +130,18 @@ void X11WindowBase::Create() {
   // type of the window.
   ui::PlatformWindowType ui_window_type = ui::PlatformWindowType::kWindow;
   // TODO(msisov, jkim): pass PlatformWindowInitProperties here.
-  if (ui_window_type != ui::PlatformWindowType::PLATFORM_WINDOW_TYPE_WINDOW) {
-    // Setting this to True, doesn't allow X server to set different
-    // properties, e.g. decorations.
-    // TODO(msisov): Investigate further.
-    // https://tronche.com/gui/x/xlib/window/attributes/override-redirect.html
-    swa.override_redirect = x11::True;
-    window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_MENU");
-  } else {
-    window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_NORMAL");
+   switch (ui_window_type) {
+    case ui::PlatformWindowType::kMenu:
+      window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_MENU");
+      swa.override_redirect = x11::True;
+      break;
+    case ui::PlatformWindowType::kPopup:
+      swa.override_redirect = x11::True;
+      window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_NOTIFICATION");
+      break;
+    default:
+      window_type = gfx::GetAtom("_NET_WM_WINDOW_TYPE_NORMAL");
+      break;
   }
 
   xwindow_ =